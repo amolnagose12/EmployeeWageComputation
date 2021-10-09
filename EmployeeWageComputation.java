0d63d15a751de8c87305b00561e4ee22e05d7c7a
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-10 02:55:17 +0530

    Added full time and part time employee wage program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,21 +3,27 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
 
-//		constants for program
-		int isPresent = 1;
+		//		constants for program
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int isFullTime = 1;
+		int isPartTime = 2;
 		int wagePerHr = 20;
-		int fullDayWorkHr = 8;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		
-//		checking condition for attendence and calculating wage
-		
-		if (isPresent == empCheck) {
-			int salary = wagePerHr * fullDayWorkHr;
+		int fullTimeWorkHr = 8;
+		int partTimeWorkHr = 4;
+	
+		//	checking condition for full time and part time and calculating wage
+	
+		if (isFullTime == empCheck) {
+			int salary = wagePerHr * fullTimeWorkHr;
 			System.out.println("Wage per day is" + " " + salary);
+		}		
+		else if (empCheck == isPartTime) {
+			int salary = wagePerHr * partTimeWorkHr;
+			System.out.println(salary);
 		}
 		else {
 			int salary = 0;
 			System.out.println("As employee is absent wage is: " + salary);
 		}
-	}	
-}
+	}
+}	
