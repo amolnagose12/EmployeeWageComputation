01ca7ad51cbaa43c8b97cfc387a32824cbe0c689
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-10-10 02:34:57 +0530

    Added employee daily wage program

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,22 +1,23 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-
 	public static void main(String[] args) {
-		
-	//constants for program
-				
-		int isEmployeePresent = 1;
-//		getting 0 & 1 random value
+
+//		constants for program
+		int isPresent = 1;
+		int wagePerHr = 20;
+		int fullDayWorkHr = 8;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-				
-//		checking employee is present or not
-				
-		if (isEmployeePresent == empCheck) {
-			System.out.println("Employee is Present");
-				}
-		else {
-			System.out.println("Employee is absent");
+		
+//		checking condition for attendence and calculating wage
+		
+		if (isPresent == empCheck) {
+			int salary = wagePerHr * fullDayWorkHr;
+			System.out.println("Wage per day is" + " " + salary);
 		}
-	}
+		else {
+			int salary = 0;
+			System.out.println("As employee is absent wage is: " + salary);
+		}
+	}	
 }
